b4ff2f1e8a898381d9360a1e0341c627cd88fee1
Sylvain <sylvain.faucon@capfi.fr>
2021-08-12 11:32:20 +0200

    Add diploma image

--- front/src/components/TokenImage/index.js
+++ front/src/components/TokenImage/index.js
@@ -1,27 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import diplome from './diplome.png';
+import "./TokenImage.css";
 
-const IMAGE_SIZE = 100;
+const IMAGE_SIZE = 10;
 
 const TokenImage = ({ outer, inner, size }) => {
   const gradId = `${outer}${inner}`.replace(/#/g, "");
   const imageSize = size || IMAGE_SIZE;
   return (
-    <svg
-      width={imageSize}
-      height={imageSize}
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        cx={imageSize / 2}
-        cy={imageSize / 2}
-        r={imageSize / 2 - 4}
-        fill={`url(#${gradId})`}
-        stroke="#fff"
-        strokeWidth="2"
-      />
-    </svg>
+      <div>
+   <div>
+     <img src={diplome} alt="Diplome" />
+   </div>
+   <div class="centered">
+      Sylvain Faucon
+    </div>
+</div>
+
   );
 };
 
